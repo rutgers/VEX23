37b36d51d18b1b3c9afebfa2eeeaaf3965f483ae
Jack Lowry <jacklowry712@gmail.com>
2023-02-18 13:45:34 -0500

    added jetson reading

--- include/auton_util.cpp
+++ include/auton_util.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <string>
+#include <cstring>
+#include <cassert>
+
 #ifndef MAIN_H
 #define MAIN_H
 #include "main.h"
@@ -11,6 +20,87 @@
 #include "vision.cpp"
 #endif
 
+char data_received[50];
+char previous_data_received[50];
+ 
+double xmin, ymin, xmax, ymax;
+bool detected = false;
+// Reads data being sent from the Jetson
+// Credited to: https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+ 
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+ 
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+ 
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+ 
+    result = (char**)malloc(sizeof(char*) * count);
+ 
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+ 
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+ 
+    return result;
+}
+ 
+bool read_from_jetson()
+{
+    char** parsed_data_received;
+    fgets(data_received, sizeof(data_received), stdin);
+ 
+    if (strcmp(data_received, previous_data_received) != 0)
+    {
+		printf(data_received);
+		if(strcmp(data_received, "RPI_NONE\n")) {
+			detected = false;
+		}
+		else {
+			parsed_data_received = str_split(data_received, '_');
+			xmin = atof(parsed_data_received[1]);
+			ymin = atof(parsed_data_received[2]);
+			xmax = atof(parsed_data_received[3]);
+			ymax = atof(parsed_data_received[4]);
+			detected = true;
+		}
+ 
+        strcpy(previous_data_received, data_received);
+        return true;
+    }
+    return false;
+}
+ 
 
 class PID_Controller {
     public:
